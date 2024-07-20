e1864b4e4af81dc0607439423b68f5756c563e70
Hindol19 <hindolbanerjee5@gmail.com>
2024-07-20 14:39:08 +0530

    intialization

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)"],
+      },
+      colors: {
+        // dark: "#1b1b1b",
+        dark: "#060608",
+        // light: "#f5f5f5",
+        light: "#fad3cf",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        "2xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        xl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        lg: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        md: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        sm: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+
+        xs: { max: "479px" },
+        // => @media (max-width: 479px) { ... }
+      },
+    },
+  },
+  plugins: [],
+};
